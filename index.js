bb6f6ea03beee9f2fc6befecdf05220aa76657df
ttttmmmmm <tam1b3a4444@gmail.com>
2024-09-14 21:58:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client(); 
-{
-  "name": "24-voice-account",
-  "version": "0.1.0",
-  "description": "24-voice-account",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "discord.js-selfbot-v13": "^3.1.4",
-    "@discordjs/voice": "^0.17.0",
-    "express": "^4.18.3"
-  },
-  "engines": {
-    "node": "16.x"
-  }
-}
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
 })
