410e2e63c17ffbdf9dad1871f526ebdef6c228e7
ttttmmmmm <tam1b3a4444@gmail.com>
2024-09-14 20:21:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client(); 
-
+{
+  "name": "24-voice-account",
+  "version": "0.1.0",
+  "description": "24-voice-account",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js-selfbot-v13": "^3.1.4",
+    "@discordjs/voice": "^0.17.0",
+    "express": "^4.18.3"
+  },
+  "engines": {
+    "node": "16.x"
+  }
+}
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
 })
